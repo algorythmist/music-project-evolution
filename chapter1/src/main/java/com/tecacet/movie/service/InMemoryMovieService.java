758119b5eddee743644f4137dfd578e363dcc331
dimitri <dimitri@trizic.com>
2017-11-12 10:31:22 -0800

    movie service

---
+++ chapter1/src/main/java/com/tecacet/movie/service/InMemoryMovieService.java
@@ -0,0 +1,214 @@
+package com.tecacet.movie.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import com.tecacet.movie.model.Genre;
+import com.tecacet.movie.model.Movie;
+import com.tecacet.movie.model.Person;
+
+/**
+ * In Memory implementation of the MovieService
+ * 
+ * @author dimitri
+ *
+ */
+public class InMemoryMovieService implements MovieService {
+
+	private final List<Movie> movies = new ArrayList<>();
+	private final Map<String, EnrichedPerson> personsByName = new TreeMap<>();
+	private final Map<String, EnrichedGenre> genresByName = new TreeMap<>();
+
+	private class EnrichedGenre implements Genre {
+		private final String name;
+		private final List<Movie> movies = new ArrayList<>();
+
+		public EnrichedGenre(String name) {
+			super();
+			this.name = name;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		public List<Movie> getMovies() {
+			return movies;
+		}
+
+		public void addMovie(Movie movie) {
+			movies.add(movie);
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
+
+	}
+
+	private class EnrichedPerson implements Person {
+		private final String name;
+
+		private boolean isActor = false;
+		private boolean isDirector = false;
+		private final List<Movie> moviesActed = new ArrayList<>();
+		private final List<Movie> moviesDirected = new ArrayList<>();
+
+		public EnrichedPerson(String name) {
+			super();
+			this.name = name;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
+
+		public boolean isActor() {
+			return isActor;
+		}
+
+		public void setActor(boolean isActor) {
+			this.isActor = isActor;
+		}
+
+		public boolean isDirector() {
+			return isDirector;
+		}
+
+		public void setDirector(boolean isDirector) {
+			this.isDirector = isDirector;
+		}
+
+		public void addMovieActed(Movie movie) {
+			moviesActed.add(movie);
+		}
+
+		public void addMovieDirected(Movie movie) {
+			moviesDirected.add(movie);
+		}
+
+		public List<Movie> getMoviesActed() {
+			return moviesActed;
+		}
+
+		public List<Movie> getMoviesDirected() {
+			return moviesDirected;
+		}
+
+	}
+
+	public InMemoryMovieService(List<? extends Movie> allMovies) {
+		for (Movie movie : allMovies) {
+			movies.add(movie);
+			registerActors(movie);
+			registerDirectors(movie);
+			registerGenres(movie);
+		}
+	}
+
+	private void registerGenres(Movie movie) {
+		for (String name : movie.getGenres()) {
+			EnrichedGenre genre = findGenre(name);
+			genre.addMovie(movie);
+		}
+	}
+
+	private void registerDirectors(Movie movie) {
+		for (String name : movie.getDirectors()) {
+			EnrichedPerson person = findPerson(name);
+			person.addMovieDirected(movie);
+			person.setDirector(true);
+		}
+	}
+
+	private void registerActors(Movie movie) {
+		for (String name : movie.getActors()) {
+			EnrichedPerson person = findPerson(name);
+			person.addMovieActed(movie);
+			person.setActor(true);
+		}
+	}
+
+	private EnrichedGenre findGenre(String name) {
+		EnrichedGenre genre = genresByName.get(name);
+		if (genre == null) {
+			genre = new EnrichedGenre(name);
+			genresByName.put(name, genre);
+		}
+		return genre;
+	}
+
+	private EnrichedPerson findPerson(String name) {
+		EnrichedPerson person = personsByName.get(name);
+		if (person == null) {
+			person = new EnrichedPerson(name);
+			personsByName.put(name, person);
+		}
+		return person;
+	}
+
+	@Override
+	public List<Movie> findMoviesWithActor(String actorName) {
+		EnrichedPerson person = personsByName.get(actorName);
+		if (person == null) {
+			return Collections.emptyList();
+		}
+		return person.getMoviesActed();
+	}
+
+	@Override
+	public List<Movie> findMoviesWithDirector(String directorName) {
+		EnrichedPerson person = personsByName.get(directorName);
+		if (person == null) {
+			return Collections.emptyList();
+		}
+		return person.getMoviesDirected();
+	}
+
+	@Override
+	public List<Movie> getAllMovies() {
+		return new ArrayList<>(movies);
+	}
+
+	@Override
+	public List<Person> getAllActors() {
+		return personsByName.values().stream().filter(EnrichedPerson::isActor).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Person> getAllDirectors() {
+		return personsByName.values().stream().filter(EnrichedPerson::isDirector).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<Genre> getAllGenres() {
+		return new ArrayList<>(genresByName.values());
+	}
+
+	@Override
+	public List<Movie> findMoviesInGenre(String genreName) {
+		EnrichedGenre genre = genresByName.get(genreName);
+		if (genre == null) {
+			return Collections.emptyList();
+		}
+		return genre.getMovies();
+	}
+
+	@Override
+	public List<Movie> findByTitle(String name) {
+		return movies.stream().filter(m -> name.equals(m.getTitle())).collect(Collectors.toList());
+	}
+
+}
---
+++ chapter1/src/test/java/com/tecacet/movie/service/InMemoryMovieServiceTest.java
@@ -0,0 +1,86 @@
+package com.tecacet.movie.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.tecacet.movie.model.Genre;
+import com.tecacet.movie.model.Movie;
+import com.tecacet.movie.model.Person;
+import com.tecacet.movie.parser.JsonMovie;
+import com.tecacet.movie.parser.MovieParser;
+
+public class InMemoryMovieServiceTest {
+
+	private static final MovieParser movieParser = new MovieParser();
+	private static MovieService movieService;
+
+	@BeforeClass
+	public static void setUp() throws IOException {
+		List<JsonMovie> movies = movieParser.parse("moviedata.json");
+		movieService = new InMemoryMovieService(movies);
+	}
+
+	@Test
+	public void getAllMovies() {
+		List<Movie> movies = movieService.getAllMovies();
+		assertEquals(4609, movies.size());
+	}
+
+	@Test
+	public void getAllGenres() {
+		List<Genre> genres = movieService.getAllGenres();
+		assertEquals(24, genres.size());
+		genres.forEach(g -> System.out.println(g));
+	}
+
+	@Test
+	public void getAllActors() {
+		List<Person> actors = movieService.getAllActors();
+		assertEquals(5265, actors.size());
+
+	}
+
+	@Test
+	public void getAllDirectors() {
+		List<Person> directors = movieService.getAllDirectors();
+		assertEquals(2333, directors.size());
+
+	}
+
+	@Test
+	public void findMoviesWithActor() throws IOException {
+		List<Movie> actorMovies = movieService.findMoviesWithActor("Laurence Olivier");
+		assertEquals(6, actorMovies.size());
+		assertEquals(
+				"[Spartacus (1960): 8.0, Clash of the Titans (1981): 6.7, Rebecca (1940): 8.3, The Bounty (1984): 6.9, Marathon Man (1976): 7.5, The Prince and the Showgirl (1957): 6.5]",
+				actorMovies.toString());
+	}
+
+	@Test
+	public void findMoviesWithDirector() {
+		List<Movie> movies = movieService.findMoviesWithDirector("Ron Howard");
+		assertEquals(20, movies.size());
+	}
+
+	@Test
+	public void findMoviesInGenre() {
+		List<Movie> movies = movieService.findMoviesInGenre("News");
+		assertEquals(1, movies.size());
+		Movie movie = movies.get(0);
+		assertEquals("Red Obsession", movie.getTitle());
+	}
+
+	@Test
+	public void findByTitle() {
+		List<Movie> movies = movieService.findByTitle("The Hunger Games: Catching Fire");
+		assertEquals(1, movies.size());
+		Movie movie = movies.get(0);
+		assertFalse(movie.getRating().isPresent());
+	}
+}
