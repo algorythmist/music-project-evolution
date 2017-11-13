6d98dbf4061cdca81cd6cda985550e40bafe096f
dimitri <dimitri@trizic.com>
2017-11-13 10:01:21 -0800

    chapter 3

---
+++ chapter3/src/main/java/com/tecacet/movie/model/Genre.java
@@ -0,0 +1,7 @@
+package com.tecacet.movie.model;
+
+public interface Genre {
+
+	String getName();
+	
+}
---
+++ chapter3/src/main/java/com/tecacet/movie/model/Person.java
@@ -0,0 +1,7 @@
+package com.tecacet.movie.model;
+
+public interface Person {
+
+	String getName();
+	
+}
