9706dc2ba18f3dc353ea83bf6932e89350e5fe34
dimitri <dimitri@trizic.com>
2017-11-13 20:02:03 -0800

    basic JPA

---
+++ chapter5/movie-project/movie-jpa/src/main/java/com/tecacet/movie/jpa/model/EntityMovie.java
@@ -0,0 +1,126 @@
+package com.tecacet.movie.jpa.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.tecacet.movie.domain.Movie;
+
+@Entity
+@Table(name="movie")
+public class EntityMovie implements Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String title;
+	private int year;
+	private LocalDate releaseDate;
+	private String plot;
+	private int duration;
+	private Double rating;
+	private String imageUrl;
+	
+	public EntityMovie(String title) {
+		super();
+		this.title = title;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public int getYear() {
+		return year;
+	}
+
+	@Override
+	public LocalDate getReleaseDate() {
+		return releaseDate;
+	}
+
+	@Override
+	public String getPlot() {
+		return plot;
+	}
+
+	@Override
+	public int getDuration() {
+		return duration;
+	}
+
+	@Override
+	public Optional<Double> getRating() {
+		return Optional.ofNullable(rating);
+	}
+
+	@Override
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setReleaseDate(LocalDate releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public List<String> getActors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getDirectors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getGenres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ chapter5/movie-project/movie-jpa/src/test/java/com/tecacet/movie/jpa/repository/MovieRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.tecacet.movie.jpa.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tecacet.movie.jpa.config.PersistanceConfiguration;
+import com.tecacet.movie.jpa.model.EntityMovie;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= {PersistanceConfiguration.class})
+@Transactional
+public class MovieRepositoryTest {
+
+	@Resource
+	private MovieRepository movieRepository;
+
+	@Test
+	public void crudOperations() {
+		List<EntityMovie> movies = movieRepository.findAll();
+		assertTrue(movies.isEmpty());
+		
+		EntityMovie movie = new EntityMovie("Elegance");
+		movie.setDuration(189);
+		movie.setReleaseDate(LocalDate.of(2012, 3, 4));
+		movieRepository.save(movie);
+		assertTrue(movie.getId() > 0);
+		
+		Optional<EntityMovie> found = movieRepository.findById(movie.getId());
+		assertEquals("Elegance", found.get().getTitle());
+		
+		List<EntityMovie> allMovies = movieRepository.findAll();
+		assertEquals(1, allMovies.size());
+		
+		movieRepository.delete(movie);
+		allMovies = movieRepository.findAll();
+		assertEquals(0, allMovies.size());
+		
+	}
+
+}
